4cd57023ec5347ff3462368c4e8d90c9b98cd853
imshrey26 <imshrey26@gmail.com>
2023-07-22 13:07:13 +0530

    setup completed

--- template.py
+++ template.py
@@ -7,7 +7,7 @@ while True:
 
     if project_name != "":
         break
-    
+
 list_of_files = [
     f"{project_name}/__init__.py",
     f"{project_name}/components/__init__.py",
@@ -34,7 +34,7 @@ for filepth in list_of_files:
     if filedir != "":
         os.makedirs(filedir,exist_ok=True)
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath,'w') as f:
             pass
     
